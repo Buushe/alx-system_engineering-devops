c652b66084b3e7ad5782bb54382c1d3440e283ab
Buushe <aadangoranyo@gmail.com>
2023-02-01 09:35:09 +0000

    0x16-api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-"""
-recursive function that queries the Reddit API and returns a list
-containing the titles of all hot articles for a given subreddit
-"""
+"""Contains recurse function"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], count=0, next_page=None):
-    """return list containing titles of all hot articles"""
-    headers = {
-        "User-Agent": "0x16. API_advanced-e_kiminza"
-    }
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    params = {"limit": 50, "next_page": next_page, "count": count}
-    response = requests.get(url, headers=headers,
-                            params=params, allow_redirects=False)
-    if response.status_code != 200:
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         return None
-    response_ = response.json().get("data")
-    next_page = response_.get("next_page")
-    count += response_.get("dist")
-    children = response_.get("children")
-    for child in children:
-        title = child.get("data").get("title")
-        hot_list.append(title)
-    if next_page is not None:
-        return recurse(subreddit, hot_list, count, next_page)
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
     return hot_list
